b2d0c4a8067f308d83786a3dadd99b9e3f8717e0
Bjørn Vedel Bennetsen <bjornvedel95@gmail.com>
2017-10-01 18:27:02 +0200

    more

--- CompSys/assignment3/test_all.sh
+++ CompSys/assignment3/test_all.sh
@@ -3,11 +3,11 @@
 num_test=$(ls programs/*/*x86 | wc -l)
 
 echo "Running ${num_test} number of test."
-for test_dir in $(ls programs/) 
+for test_dir in $(ls test_programs/) 
 do
     test_i=1
     echo "Testing \"${test_dir}\" commands:"
-    for test_program in $(ls programs/$test_dir/*x86)
+    for test_program in $(ls test_programs/$test_dir/*x86)
     do				    	    	    
 	raw=$(cut -d'.' -f1 <<< $test_program)
 	name=$(cut -d'/' -f3 <<< $raw)
