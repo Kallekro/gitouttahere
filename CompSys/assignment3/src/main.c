f1e246ef00edfd86f15e69d2794136e12bbc7abf
Kalle Kromann <kallekro@gmail.com>
2017-10-01 15:07:18 +0200

    commmmmmmon

--- CompSys/assignment3/src/main.c
+++ CompSys/assignment3/src/main.c
@@ -69,32 +69,45 @@ int main(int argc, char* argv[]) {
         val major_op = pick_bits(4,4, inst_word);
         val minor_op = pick_bits(0,4, inst_word);
         
+        // booleans for different move instructions
         bool is_RtoRmove = is(MOV_RtoR, major_op);
         bool is_ItoRmove = is(MOV_ItoR, major_op);
         bool is_RtoMmove = is(MOV_RtoM, major_op);
         bool is_MtoRmove = is(MOV_MtoR, major_op);
 	
+        // boolean for arithmetic operations
         bool is_Arithmetic = is(ARITHMETIC, major_op);
 
+        // booleans for stack manipulation operations
         bool is_Push = is(PUSH, major_op);
         bool is_Pop = is(POP, major_op);
 
+        // booleans for call and return
         bool is_Call = is(CALL, major_op);     
         bool is_Return = is(RET, major_op);
 	
+        // boolean for jump instruction 
         bool is_Jump = is(JCC, major_op);
+        // if program should actually jump depends on current condition codes
         bool do_Jump = is_Jump && eval_condition(cc, minor_op); 	
-
+        
+        // booleans for nop and halt instructions
         bool is_Nop = is(NOP, major_op);
         bool is_Halt = is(HLT, major_op);
 
+        // these three instructions have size 1
         bool is_Small = is_Nop || is_Halt || is_Return;
 
+        // these instructions are move instructions
         bool is_move = is_RtoRmove || is_ItoRmove || is_RtoMmove || is_MtoRmove;
+        // these instructions contain registers
       	bool has_regs = is_move || is_Arithmetic || is_Push || is_Pop;
-        bool mov_to_mem = is_RtoMmove;
-        bool has_imm = is_ItoRmove || mov_to_mem || is_MtoRmove || is_Jump || is_Call;
+        // these instructions contain immediate values
+        bool has_imm = is_ItoRmove || is_RtoMmove || is_MtoRmove || is_Jump || is_Call;
+        // these instructions are stack operations
 	      bool stack_op = is_Push || is_Pop || is_Call || is_Return;	
+
+        // Find size
       	val size = or( use_if(is_Small, from_int(1)),
                        use_if(!is_Small, or(use_if(has_imm, from_int(6)),
                                             use_if(!has_imm, from_int(2)))));
@@ -102,25 +115,31 @@ int main(int argc, char* argv[]) {
         size = or(use_if(is_Jump || is_Call, from_int(5)),
                   use_if(!is_Jump && !is_Call, size));	
 
-        val reg_a = or(use_if(is_MtoRmove, pick_bits(8,4, inst_word)),
-                       use_if(!is_MtoRmove, pick_bits(12,4, inst_word)));
+        bool flipped = is_MtoRmove || (is_Arithmetic && is(0x4, minor_op));
+        // find register a in instruction
+        // the registers are flipped in memory to register move
+        val reg_a = or(use_if(flipped, pick_bits(8,4, inst_word)),
+                       use_if(!flippeD, pick_bits(12,4, inst_word)));
 
+        // find instruction b in instruction
+        // the registers are flipped in memory to register move
         val reg_b = or(use_if(is_MtoRmove, pick_bits(12,4, inst_word)),
                        use_if(!is_MtoRmove, pick_bits(8,4, inst_word)));
 
+        // if the operation is a stack operation the target register aka. register b is the stack pointer register
 	      reg_b = or(use_if(stack_op, REG_SP),
 		               use_if(!stack_op, reg_b));	
 
+        // find the immediate bytes in the instruction
 	      val imm_bytes = or( use_if(!has_regs, pick_bits(8, 32, inst_word)),
                             use_if(has_regs, pick_bits(16, 32, inst_word)));
-        val imm = or(use_if(has_imm, imm_bytes),
-		                 use_if(!has_imm, from_int(0)));
-	
-        val sign_extended_imm = sign_extend(31,imm);
 
-        val next_inst_pc = add(pc, size);
+        // sign extend the immediate bytes
+        // currently the value is only occupying half of the 8 bytes in val
+        // so occupy the rest with the correct sign
+        val sign_extended_imm = sign_extend(31,imm_bytes);
 
-        val next_jmp_inst_pc = sign_extended_imm;
+        val next_inst_pc = add(pc, size); 
 
         stop = is_Halt;
         
@@ -128,61 +147,84 @@ int main(int argc, char* argv[]) {
         val op_b;
         // execute
 	
-        op_a = memory_read(regs, 1, reg_a, true);
+        op_a = memory_read(regs, 0, reg_a, true);
+        op_b = memory_read(regs, 1, reg_b, true);
+        // store the value in the register
+        // because pop needs it after operand a has been altered
         val reg_a_val = op_a;
 
+        // if imm to reg operand operand a is the immediates
        	op_a = or(use_if(is_ItoRmove, sign_extended_imm),
-		    use_if(!is_ItoRmove, op_a));
+		              use_if(!is_ItoRmove, op_a));
 
+        // if push or call, operand a is -8
+        // if pop or return, operand a is 8
+        // this is the value used to increment or decrement the stack pointer
         op_a = or(use_if(!stack_op, op_a),
                   use_if(stack_op, or(use_if(is_Push || is_Call,   from_int(-8)),
                                       use_if(!is_Push && !is_Call, from_int(8)))));
 	
-        op_b = memory_read(regs, 0, reg_b, true);
-	
+        // if arithmentic use the correct alu method, specified in minor_op
+        // else be sure to use add (to decrement/increment the stack pointer) 	
         val alu_method = or(use_if(is_Arithmetic, minor_op),
-                            use_if(!is_Arithmetic, from_int(0))); // Set to add
+                            use_if(!is_Arithmetic, from_int(0))); // 0 = add
 
+        // execute the alu and get both the result and the condition codes
         alu_execute_result alu_res = alu_execute(alu_method, op_a, op_b);
 
         // Set condition codes
+        // the macro we use will set the codes to be the codes from the alu result if the instruction is an arithmetic operation
+        // and if the instruction is not an arithmetic operation the condition codes will remain as they were
         cc.of = CCParser(cc.of, is_Arithmetic, alu_res.cc.of); 
         cc.sf = CCParser(cc.sf, is_Arithmetic, alu_res.cc.sf);
         cc.zf = CCParser(cc.zf, is_Arithmetic, alu_res.cc.zf);
         
 	      // select result for register update
+        // if either arithmetic or stack instruction we use the result from the alu execution
+        // this is either the decremented pointer or the result of an arithmetic operation
+        // if neither arithmetic or stack instruction the result is either the immediate or the register value being moved
         val datapath_result_reg = or(use_if(is_Arithmetic || stack_op, alu_res.result),
 			                               use_if(!is_Arithmetic && !stack_op, op_a));
 
-        datapath_result_reg = or(use_if(is_MtoRmove,memory_read(mem,1,add(op_a, sign_extended_imm),is_MtoRmove)),
+        // if memory to register the memory needs to be read and stored in the register update result
+        datapath_result_reg = or(use_if(is_MtoRmove, memory_read(mem, 2, add(op_a, sign_extended_imm), is_MtoRmove)),
                                  use_if(!is_MtoRmove,datapath_result_reg));
 
+        // select result for memory update
+        // there are three different scenarios for memory update, 
+        // register to memory uses the value in the register
+        // push also uses this value, but has overwritten op_a
 	      val datapath_result_mem = or(use_if(is_RtoMmove, op_a),
                                      use_if(!is_RtoMmove, or(use_if(is_Push, reg_a_val), 
                                                              use_if(!is_Push, sign_extended_imm))));
-        
+        // call writes the next instruction so return can return to here 
         datapath_result_mem = or(use_if(is_Call, next_inst_pc),
                                  use_if(!is_Call, datapath_result_mem));
         
-        // pick result value and target register        
-        bool reg_wr_enable =((is_move && !mov_to_mem) && eval_condition(cc, minor_op));
-        reg_wr_enable = reg_wr_enable || (is_Arithmetic && !is(0x4, minor_op));
-        reg_wr_enable = reg_wr_enable || stack_op;
-	
-        bool mem_wr_enable = (is_move && mov_to_mem) || is_Push || is_Call;
+        // set register write enable      
+        // if move to register and the conditions are met (for movq the minor_op is 0 therefor always move)
+        bool reg_wr_enable =((is_move && !is_RtoMmove) && eval_condition(cc, minor_op));
+        // if arithmetic operation and not compare or stack operation
+        reg_wr_enable = reg_wr_enable || (is_Arithmetic && !is(0x4, minor_op)) || stack_op;
+        
+        // set memory write enable
+        // only these three instructions need to write to memory  
+        bool mem_wr_enable = is_RtoMmove || is_Push || is_Call;
 
+        // we chose the standard that register b is the target register
         val target_reg = reg_b;
 
+        // target memory is either a displaced address or the next free space on the stack
         val target_mem = or(use_if(is_RtoMmove, add(op_b, sign_extended_imm)), 
                             use_if(!is_RtoMmove, add(op_b, from_int(-8))));
 
-        val top_stack = memory_read(regs, 2, REG_SP, is_Pop || is_Return);
+        // if pop or return read value at the top of the stack 
+        val top_stack = memory_read(mem, 2, memory_read(regs, 2, REG_SP, is_Pop || is_Return), is_Pop || is_Return);
 	
         // determine PC for next cycle
-        val next_pc = or (use_if(do_Jump || is_Call, next_jmp_inst_pc),
-                          use_if(!do_Jump && !is_Call, next_inst_pc));
-        next_pc = or (use_if(is_Return, memory_read(mem, 0, top_stack, is_Return)),
-                          use_if(!is_Return, next_pc));
+        val next_pc = or( use_if(do_Jump || is_Call, sign_extended_imm),
+                          use_if(!do_Jump && !is_Call, or( use_if(is_Return, top_stack), 
+                                                           use_if(!is_Return, next_inst_pc))));
 
         // potentially pretty-print something to show progress before
         // ending cycle and overwriting state from start of cycle:
@@ -191,8 +233,10 @@ int main(int argc, char* argv[]) {
           unsigned int byte = (inst_word.val >> (8*j)) & 0xff;
             printf("%x ", byte);
         }
-        if (reg_wr_enable)
+        if (reg_wr_enable) {
+            if (size.val != 6 || size.val != 5) printf("\t");
             printf("\t\tr%ld = %lx\n", target_reg.val, datapath_result_reg.val);
+        }
         else
             printf("\n");
 
@@ -204,18 +248,19 @@ int main(int argc, char* argv[]) {
             // on it, all values are easy to inspect with a debugger.
             validate_pc_wr(tracer, instruction_number, next_pc);
 
-	    if (reg_wr_enable) {
-	      validate_reg_wr(tracer, instruction_number, target_reg, datapath_result_reg);
-	    }
+	          if (reg_wr_enable) {
+	            validate_reg_wr(tracer, instruction_number, target_reg, datapath_result_reg);
+	          }
  
-	    if (mem_wr_enable) {
-	      validate_mem_wr(tracer, instruction_number, target_mem, datapath_result_mem);
-	    }
+	          if (mem_wr_enable) {
+	            validate_mem_wr(tracer, instruction_number, target_mem, datapath_result_mem);
+	          }
         }
 
         // store results at end of cycle
         pc = next_pc;
         memory_write(regs, 0, target_reg, datapath_result_reg, reg_wr_enable);
+        // pop needs to write to two registers, the target from instruction and the stack pointer
         memory_write(regs, 1, reg_a, top_stack, is_Pop);
         memory_write(mem, 0, target_mem, datapath_result_mem, mem_wr_enable);
     }
