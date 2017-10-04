b8bb30a65ee344e0c1b69304aa04c6bc6454dd2a
Kalle Kromann <kallekro@gmail.com>
2017-10-04 21:27:25 +0200

    mainss

--- 2048/src/graphics.c
+++ 2048/src/graphics.c
@@ -11,53 +11,6 @@ int randInt(int lo, int hi) {
   return ((rand() % hi) + lo);
 }
 
-// int main() {
-// 
-//   time_t t;
-//   srand((unsigned) time(&t));
-// 
-//   InitializeGame();
-// 
-//   int max_y, max_x; 
-//   getmaxyx(stdscr, max_y, max_x);
-// 
-//   int ch;
-// 
-//   ch = getch();
-//   if (ch == KEY_F(1)) {
-//     printw("F1 Key pressed\n");
-//   } 
-//   else {
-//     printw("Pressed key: %c", ch);
-//   }
-// 
-//   int dim = 4;
-//   
-//   int** arr;
-//   arr = malloc(sizeof(*arr) * dim);
-// 
-//   int numbers[6] = {0, 2, 4, 8, 16, 32};
-// 
-//   for (int i=0; i<dim; i++) {
-//     arr[i] = malloc(sizeof(int) * dim);
-// 
-//     for (int j=0; j<dim; j++) {
-//       arr[i][j] = numbers[randInt(0, 6)];
-//     }
-//   }
-// 
-//   PrintArray(arr, dim, max_y, max_x);
-// 
-//   for (int i=0; i<dim; i++) {
-//     free(arr[i]);
-//   }
-//   free(arr);
-//   refresh();
-//   getch();
-//   endwin();
-// 
-//   return 0;
-// }
 
 int InitializeGame() {
   initscr();
