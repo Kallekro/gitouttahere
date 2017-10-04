4b7415bcb9f930c957231a92b6d33764d2c139d7
Bjørn Vedel Bennetsen <bjornvedel95@gmail.com>
2017-10-04 20:36:58 +0200

    push

--- 2048/src/core.c
+++ 2048/src/core.c
@@ -2,21 +2,22 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
-#include<bool.h>
+#include<stdbool.h>
+#include<ncurses.h>
 
 enum moveType {Up, Down, Left, Right};
 
 int randInt(int, int);
 void printArray(int**, int);
 void initialize_with_val(int**, int, int);
-
+void feed_board(int**, int dim);
 
 int main (int argc, char* argv[]) {
 
   if (argc != 2) {
     printf("usage: dim");
   }
-
+    
   time_t t;
   srand((unsigned) time(&t));
   
@@ -24,6 +25,8 @@ int main (int argc, char* argv[]) {
   
   int i;
   int** arr;
+
+  //enum moveType keyPress;
   
   // allocate memory for array
   arr = malloc(dim * sizeof(*arr));
@@ -31,11 +34,20 @@ int main (int argc, char* argv[]) {
     arr[i] = malloc(dim * sizeof(*arr));
   }
 
-  // initialize game board with zeros.
+  // initialize game board with zeros and feed it twice.
   initialize_with_val(arr, dim, 0);
+  feed_board(arr, dim); feed_board(arr, dim);
+  printArray(arr, dim);
+  
+  bool game_running = true;
 
   
-  printArray(arr, dim);
+  while (game_running) { 
+
+    char c = 0 ;
+    c = getch();
+    printf("%c", c);
+  }
   
   // Free memory for array.
   for (i=0; i<dim; i++) {
@@ -46,22 +58,23 @@ int main (int argc, char* argv[]) {
   exit(EXIT_SUCCESS);
 }
 
-void feed_board(int** arr, int dim) {
-  bool inserted = false;
 
-  int feeds[2] = {2,4};
+
+void feed_board(int** arr, int dim) {
+  int feed[2] = {2,4};
+  bool inserted = false;
   int col_guess, row_guess;
-  int rndIndex = randInt(0,1);
+  int rnd_feedval = randInt(0,2);
 
   while (!inserted) {
-    col_guess = randInt(0,dim);row_guess = randInt(0,dim);
+    col_guess = randInt(0,dim);
+    row_guess = randInt(0,dim);
     
-    if (arr[row_guess, col_guess] != 0) {
-      arr[row_guess, col_guess] = feeds[rndIndex];
+    if (arr[row_guess][col_guess] == 0) {
+      arr[row_guess][col_guess] = feed[rnd_feedval];
       inserted = true;
     }  
   }
-  printf("Inserted the value %d, at index (%d, %d)", feeds[rndIndex], row_guess, col_guess);
 }
 
 
