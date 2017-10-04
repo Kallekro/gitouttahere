041f8bb992c295bbf8f2b1c71285457dc16e0725
Kalle Kromann <kallekro@gmail.com>
2017-10-04 23:50:16 +0200

    yea boii

--- 2048/src/graphics.c
+++ 2048/src/graphics.c
@@ -17,7 +17,7 @@ int max_y, max_x;
 int cellSize;
   
 int start_x;  
-int y_pos; 
+int start_y;
 
 int InitializeGame(int _dim) {
   dim = _dim;
@@ -30,14 +30,14 @@ int InitializeGame(int _dim) {
   getmaxyx(stdscr, max_y, max_x);
   cellSize = 9;
   start_x = max_x / 2 - dim * cellSize / 2;
-  y_pos = max_y/2 - dim*2/2;
+  start_y = max_y/2 - dim;
 
-  if (start_x <= 0 || y_pos <= 0) {
+  if (start_x <= 0 || start_y <= 0) {
     printw("Game does not fit on this screen. Please resize and restart");
     return 1;
   }
 
-  main_win = create_new_window(10, 10, 10, 10);
+  main_win = create_new_window(dim * 3-1, dim * cellSize + 4, start_y, start_x-2);
   return 0;
 }
 
@@ -45,32 +45,43 @@ WINDOW *create_new_window(int height, int width, int starty, int startx) {
   WINDOW *new_win;
 
   new_win = newwin(height, width, starty, startx);
-  box(new_win, 0, 0);
   wrefresh(new_win);
 
   return new_win;    
 }
 
 int PrintArray(int** arr) {
+  refresh();
   char buffer[cellSize];
+  int local_x = 2;
+  int y_pos = 2;
 
   for (int i=0; i < dim; i++) {
-    y_pos += 2;
-    move(y_pos, start_x);
+    wmove(main_win, y_pos, local_x);
     for (int j=0; j < dim; j++) {
+      if (arr[j][i] == 0) { 
+        wprintw(main_win, "%*s", cellSize, " ");
+        continue;
+      }
       sprintf(buffer, "%d", arr[j][i]);
       if (strlen(buffer) % 2 == 0) {
-        printw("%*s%s%*s", (cellSize - strlen(buffer))/2, " ",
+        wprintw(main_win,
+               "%*s%s%*s", (cellSize - strlen(buffer))/2, " ",
                            buffer,
                            (cellSize - strlen(buffer))/2 + 1, " ");
       }
       else {
-        printw("%*s%s%*s", (cellSize - strlen(buffer))/2, " ",
+        wprintw(main_win, 
+                "%*s%s%*s", (cellSize - strlen(buffer))/2, " ",
                            buffer,
                            (cellSize - strlen(buffer))/2, " ");
       }
     }
+    y_pos += 2;
   }
+
+  box(main_win, 0, 0);
+  wrefresh(main_win);
 
   return 0;
 }
--- 2048/src/main.c
+++ 2048/src/main.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <time.h>
 
+int randInt(int lo, int hi);
+
+int gameLoop(int**arr);
+
 int randInt(int lo, int hi) {
   return ((rand() % hi) + lo);
 }
@@ -14,56 +18,64 @@ int main () {
   srand((unsigned) time(&t));
 
   int dim = 4;
-  InitializeGame(dim);
 
-  int ch;
-
-  ch = getch();
-  if (ch == KEY_F(1)) {
-    printw("F1 Key pressed\n");
-  } 
-  else {
-    switch (ch) {
-      case 10: // Enter key
-        break;
-      case KEY_LEFT :
-        break;
-      case KEY_RIGHT :  
-        break;
-      case KEY_UP :
-        break;
-      case KEY_DOWN:
-        break;
-      default:  
-        break;
-    }
-  }
-  
-
-  
   int** arr;
-  arr = malloc(sizeof(*arr) * dim);
 
-  int numbers[6] = {0, 2, 4, 8, 16, 32};
+  arr = malloc(sizeof(*arr) * dim);
 
   for (int i=0; i<dim; i++) {
     arr[i] = malloc(sizeof(int) * dim);
+  }
 
+  int numbers[15] = {0, 0, 0, 0, 2, 4, 8, 16, 32, 64,
+                     128, 256, 512, 1024, 2048};
+  for (int i=0; i<dim; i++) {
     for (int j=0; j<dim; j++) {
-      arr[i][j] = numbers[randInt(0, 6)];
+      arr[i][j] = numbers[randInt(0, 15)];
     }
   }
 
-  //create_new_window(10, 10, 10, 10);
-  PrintArray(arr);
-  refresh();
-  getch();
+  InitializeGame(dim);
+  gameLoop(arr);
+  endwin();
 
   for (int i=0; i<dim; i++) {
     free(arr[i]);
   }
   free(arr);
-  endwin();
 
   return 0;
 }
+
+int gameLoop (int** arr) {
+
+  int ch;
+
+  while (1) {
+    PrintArray(arr);
+
+    ch = getch();
+    if (ch == KEY_F(1)) {
+      break;
+    } 
+    else {
+      switch (ch) {
+        case 10: // Enter key
+          break;
+        case KEY_LEFT :
+          break;
+        case KEY_RIGHT :  
+          break;
+        case KEY_UP :
+          break;
+        case KEY_DOWN:
+          break;
+        default:  
+          break;
+      }
+    }
+    
+  }
+  return 0;
+}
+
