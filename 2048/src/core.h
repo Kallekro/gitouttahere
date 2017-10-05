5d82bdcf5953101df8b499cc29e578b5301e5982
Kalle Kromann <kallekro@gmail.com>
2017-10-05 12:32:54 +0200

    core header

---
+++ 2048/src/core.h
@@ -0,0 +1,8 @@
+#ifndef CORE_H
+#define CORE_H
+
+void initialize_with_val(int** arr, int dim, int val);
+void move_board(int** arr, int dim, int move);
+void feed_board(int** arr, int dim)
+
+#endif
