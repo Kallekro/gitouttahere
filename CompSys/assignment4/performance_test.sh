671a6b630a3634de0707addf692dc2db0ed45f11
Kalle Kromann <kallekro@gmail.com>
2017-10-27 00:36:32 +0200

    quickss

--- CompSys/assignment4/performance_test.sh
+++ CompSys/assignment4/performance_test.sh
@@ -5,15 +5,17 @@ heap_1="examples/heapsort.x64"
 heap_2="examples/heapsort-2.x64"
 merge="examples/mergesort.x64"
 
+quicksort="asm_programs/quicksort.x64"
+
 ## Matrix multiplication
 
 ## temporary copy of x64-source file. Used for changing n.
 cp $mat_mul matmul_n
 
 ## Create files to hold simulation data.
-echo -n > matmul_data1.data
-echo -n > matmul_data2.data
-echo -n > matmul_data3.data
+echo -n > data_files/matmul_data1.data
+echo -n > data_files/matmul_data2.data
+echo -n > data_files/matmul_data3.data
 
 ## tmp files
 touch out_tmp.o
@@ -44,12 +46,12 @@ do
     sed -i 's/^.*\.rand 1234 .*$/'"$new_rand"'/' matmul_n
 
     ## Machine 1
-    get_cycles matmul_n matmul_data1.data
+    get_cycles matmul_n data_files/matmul_data1.data
     ## Machine 2
-    get_cycles matmul_n matmul_data2.data "-pw=4"
+    get_cycles matmul_n data_files/matmul_data2.data "-pw=4"
     ## Machine 3
-    get_cycles matmul_n matmul_data3.data "-pw=4 -ooo"
-    
+    get_cycles matmul_n data_files/matmul_data3.data "-pw=4 -ooo"
+
 done
 rm matmul_n
 
@@ -61,42 +63,50 @@ cp $heap_1 heap_1_n
 cp $heap_2 heap_2_n
 cp $merge merge_n
 
+# our quicksort
+cp $quicksort quicksort_n
 
-## Create files to hold simulation data.
-echo -n > heap_1_n_data1.data
-echo -n > heap_1_n_data2.data
-echo -n > heap_1_n_data3.data
 
-echo -n > heap_2_n_data1.data
-echo -n > heap_2_n_data2.data
-echo -n > heap_2_n_data3.data
+## Create or overwrite files to hold simulation data.
+echo -n > data_files/heap_1_n_data1.data
+echo -n > data_files/heap_1_n_data2.data
+echo -n > data_files/heap_1_n_data3.data
 
-echo -n > merge_n_data1.data
-echo -n > merge_n_data2.data
-echo -n > merge_n_data3.data
+echo -n > data_files/heap_2_n_data1.data
+echo -n > data_files/heap_2_n_data2.data
+echo -n > data_files/heap_2_n_data3.data
 
-for file in heap_1_n heap_2_n merge_n
+echo -n > data_files/merge_n_data1.data
+echo -n > data_files/merge_n_data2.data
+echo -n > data_files/merge_n_data3.data
+
+echo -n > data_files/quicksort_n_data1.data
+echo -n > data_files/quicksort_n_data2.data
+echo -n > data_files/quicksort_n_data3.data
+
+for file in heap_1_n heap_2_n merge_n quicksort_n
 do
     printf "\nTesting file: $file.x64\n"
-    for n in {500..10000..500}
+    for n in {1000..10000..1000}
     do
-	printf "n=$n\n"
-	new_dim="    .quad $n"
-	new_rand="    .rand 0x1234 $n"
-	sed -i 's/^.*\.quad .*$/'"$new_dim"'/' $file  
-	sed -i 's/^.*\.rand 1234 .*$/'"$new_rand"'/' $file
-	
-	# Machine 1
-	output=$(printf "%s_data1.data" $file)
-        get_cycles $file $output 
+	    printf "n=$n\n"
+	    new_dim="    .quad $n"
+	    new_rand="    .rand 1234 $n"
+	    sed -i 's/^.*\.quad .*$/'"$new_dim"'/' $file  
+	    sed -i 's/^.*\.rand 1234 .*$/'"$new_rand"'/' $file
+	    
+	    # Machine 1
+	    output=$(printf "%s_data1.data" $file)
+      get_cycles $file data_files/$output 
 
-	# Machine 2
-	output=$(printf "%s_data2.data" $file)
-	get_cycles $file $output "-pw=4"
+	    # Machine 2
+	    output=$(printf "%s_data2.data" $file)
+	    get_cycles $file data_files/$output "-pw=4"
 
-	# Machine 3
-        output=$(printf "%s_data3.data" $file)
-        get_cycles $file $output "-pw=4 -ooo"	
+	    # Machine 3
+      output=$(printf "%s_data3.data" $file)
+      get_cycles $file data_files/$output "-pw=4 -ooo"	
+      cat merge_n
     done
 done
 
@@ -104,10 +114,12 @@ rm heap_1_n
 rm heap_2_n
 rm merge_n
 
+rm quicksort_n
+
 rm out_tmp.o
 rm data_tmp 
 
 ## Move datafiles to data-dir
-mv *data data_files/
+#mv *data data_files/
 
 
