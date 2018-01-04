fec8067e2f02fb630a112aa88c870961b84d9c91
Kalle Kromann <kallekro@gmail.com>
2018-01-04 14:15:58 +0100

    yo

---
+++ CompSys/assignments/assignment7/src/socklib.h
@@ -0,0 +1,44 @@
+#ifndef SOCK_LIB
+#define SOCK_LIB
+
+// Function to receive all data on a non-blocking socket
+int recv_all(int sock, char* buf, int buflen) {
+  int bytesreceived = 0;
+  int initbytes = 4;
+  while (bytesreceived < initbytes) {
+    int received = recv(sock, buf+bytesreceived, buflen, 0);
+    if (received > 0) {
+      bytesreceived += received;
+    } else if (received == 0) {
+      // some error (server hung)
+      return -1;
+    }
+  }
+  char size[4];
+  strncpy(size, buf, 4);
+  int bytesremain = atoi(size) - (bytesreceived - 4);
+  while (bytesremain > 0) {
+    int received = recv(sock, buf+bytesreceived, buflen, 0);
+    if (received > 0) {
+      bytesremain -= received;
+      bytesreceived += received;
+    }
+  }
+  return bytesreceived;
+}
+
+// Function to send all data on a non-blocking socket
+int send_all(int sock, char* data, int datalen) {
+  int total_bytes = 0;
+  while (total_bytes < datalen) {
+    int sent_bytes = send(sock, data+total_bytes, datalen-total_bytes, 0);
+    if (sent_bytes == -1) {
+      return -1;
+    }
+    total_bytes += sent_bytes;
+  }
+  return total_bytes;
+}
+
+
+#endif
